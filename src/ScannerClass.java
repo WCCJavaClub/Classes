bc612d4919b87fae1922b61cea1990b61bfbf31a
Jonathan Lovelace <kingjon3377@gmail.com>
2015-11-03 22:25:09 -0500

    Add final keyword to main() arguments.
    It's never a good idea to reassign a function's arguments (since this
    cannot have any effect outside the function, but that fact isn't obvious
    from reading the code). Adding the final keyword makes attempting to do
    so a compile-time error instead of a subtle runtime bug. Because of
    this, I have Eclipse set up to automatically add the keyword to every
    function's arguments whenever I save.

--- src/ScannerClass.java
+++ src/ScannerClass.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 // comment
 public class ScannerClass {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		Scanner s = null;
 		File ScannerFile = null;
 		FileInputStream fis = null;
