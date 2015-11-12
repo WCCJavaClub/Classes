d61548181b599441d5c7f37e8abf4e1d02a5ea9b
Jonathan Lovelace <kingjon3377@gmail.com>
2015-11-12 15:29:44 -0500

    Split and extend Map tests.

--- src/testing/TestCollections.java
+++ src/testing/TestCollections.java
@@ -18,15 +18,23 @@ import org.junit.Test;
  */
 public class TestCollections {
 	/**
+	 * Test that a queue is initially empty.
+	 */
+	@Test
+	public void testQueueInitialEmptiness() {
+//		final Queue<String> queue = new MyLinkedQueue<>();
+		final Queue<String> queue = new LinkedList<>();
+		assertEquals("A queue is initially empty", 0, queue.size());
+		assertTrue("A queue is initially empty", queue.isEmpty());
+		assertFalse("An empty queue does not contain an element", queue.contains("string"));
+	}
+	/**
 	 * Test my Queue implementation.
 	 */
 	@Test
 	public void testQueue() {
 //		final Queue<String> queue = new MyLinkedQueue<>();
 		final Queue<String> queue = new LinkedList<>();
-		assertEquals("A queue is initially empty", 0, queue.size());
-		assertTrue("A queue is initially empty", queue.isEmpty());
-		assertFalse("An empty queue does not contain an element", queue.contains("string"));
 		queue.offer("string");
 		assertEquals("After adding one element, a queue has size 1", 1, queue.size());
 		assertFalse("After adding ane element, a queue is no longer empty", queue.isEmpty());
@@ -42,13 +50,40 @@ public class TestCollections {
 		assertTrue("After remove(), the queue is empty again", queue.isEmpty());
 	}
 	/**
+	 * Test that my Map implementation is initially empty.
+	 */
+	@Test
+	public void testMapInitialEmptiness() {
+//		final Map<String, String> map = new MyHashMap<>();
+		final Map<String, String> map = new HashMap<>();
+		assertEquals("A Map starts out empty", 0, map.size());
+		assertTrue("A Map starts out empty", map.isEmpty());
+	}
+	/**
 	 * Test my Map implementation.
 	 */
 	@Test
 	public void testMap() {
 //		final Map<String, String> map = new MyHashMap<>();
 		final Map<String, String> map = new HashMap<>();
-		assertEquals("A Map starts out empty", 0, map.size());
-		assertTrue("A Map starts out empty", map.isEmpty());
+		map.put("key", "value");
+		map.put("keyTwo", "valueTwo");
+		assertFalse("A Map is no longer empty after inserting items", map.isEmpty());
+		assertEquals("The Map has the right number of items", 2, map.size());
+		assertEquals("Returns right value for first key", "value", map.get("key"));
+		assertEquals("Map returns right value for different key", "valueTwo", map.get("keyTwo"));
+	}
+	/**
+	 * Test that a Map handles value replacement properly.
+	 */
+	@Test
+	public void testMapValueReplacement() {
+//		final Map<String, String> map = new MyHashMap<>();
+		final Map<String, String> map = new HashMap<>();
+		assertNull("Map returns null for a key it does not have", map.get("key"));
+		map.put("key", "value");
+		assertEquals("The Map has the first value once it is inserted", "value", map.get("key"));
+		map.put("key", "another");
+		assertEquals("The Map has the second value instead of the first after another put()", "another", map.get("key"));
 	}
 }
